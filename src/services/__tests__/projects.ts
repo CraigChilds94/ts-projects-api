3ebe0a45ed2d3e3165a924ac4ea5b72a0324b30e
Craig Childs <childscraig17@gmail.com>
2021-02-07 09:50:20 +0000

    Test json response

--- src/services/__tests__/projects.ts
+++ src/services/__tests__/projects.ts
@@ -7,6 +7,17 @@ describe('GET /projects', () => {
     it('Gets the list of projects', done => {
         request.get('/projects')
             .expect('Content-Type', /json/)
-            .expect(200, done)
+            .expect(200, done).expect({
+                "projects": [
+                    {
+                        id: 1,
+                        name: "Website Rebuild"
+                    },
+                    {
+                        id: 3,
+                        name: "Design Portfolio"
+                    }
+                ]
+            })
     })
 })
